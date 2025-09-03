bf8db05be9f29793215727cac44687e9fb3356ee
Ojetunde Elijah <ojetundeelijah2020@gmail.com>
2025-09-03 16:45:07 -0700

    Sitch case statement

--- Relational.java
+++ Relational.java
@@ -1,24 +1,25 @@
-public class Relational{
-    public static void main (String args[])
-    {
-        // int x = 6;
-        // int y = 7;
-        // int a = 5;
-        // int b = 9;
+// public class Relational{
+//     public static void main (String args[])
+//     {
+//         // int x = 6;
+//         // int y = 7;
+//         // int a = 5;
+//         // int b = 9;
 
 
-        // boolean result = (x < y) && (a < b);
-        // System.out.println(result);
-        int x = 8;
-
-        if(x> 10){
-            System.out.println("X is greater than 10");
-        }
-        else if(x == 8){
-            System.out.println("X is equal to 8");
-        }
-        else{
-            System.out.println("X is less than 10");
-        }
-    }
-}
+//         // boolean result = (x < y) && (a < b);
+//         // System.out.println(result);
+//         int x = 8;
+//         int result = (x > 10) ? 1 : 0;
+//         System.out.println(result);
+//         if(x> 10){
+//             System.out.println("X is greater than 10");
+//         }
+//         else if(x == 8){
+//             System.out.println("X is equal to 8");
+//         }
+//         else{
+//             System.out.println("X is less than 10");
+//         }
+//     }
+// }
---
+++ Switch.java
@@ -0,0 +1,21 @@
+class Switch
+{
+    public static void main (String args[])
+    {
+        int n = 1;
+        switch(n)
+        {
+            case 0:
+                System.out.println("n is zero");
+                break;
+            case 1:
+                System.out.println("n is one");
+                break;
+            case 2:
+                System.out.println("n is two");
+                break;
+            default:
+                System.out.println("n is greater than two");
+        }
+    }
+}
