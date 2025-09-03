1d4599eeb45d356e3f59eb17fee8e8ad42a7a6ee
Ojetunde Elijah <ojetundeelijah2020@gmail.com>
2025-09-03 16:38:26 -0700

    If else statement

--- Relational.java
+++ Relational.java
@@ -1,10 +1,24 @@
 public class Relational{
     public static void main (String args[])
     {
-        int x = 6;
-        int y = 7;
+        // int x = 6;
+        // int y = 7;
+        // int a = 5;
+        // int b = 9;
 
-        boolean result = x < y;
-        System.out.println(result);
+
+        // boolean result = (x < y) && (a < b);
+        // System.out.println(result);
+        int x = 8;
+
+        if(x> 10){
+            System.out.println("X is greater than 10");
+        }
+        else if(x == 8){
+            System.out.println("X is equal to 8");
+        }
+        else{
+            System.out.println("X is less than 10");
+        }
     }
 }
