dc69e1cbfb5064a0ec70271f9d1476da03929664
Ojetunde Elijah <ojetundeelijah2020@gmail.com>
2025-09-03 15:42:19 -0700

    Type casting and conversion

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        // byte b = 127;
+        int a = 257;
+        byte k = (byte) a;
+
+        float f = 10.5f;
+        int i = (int) f;
+        System.out.println(a);
+    }
+}
