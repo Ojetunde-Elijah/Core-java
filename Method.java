5b82be0619a854fbbc44bf9c307c7b7005130060
Ojetunde Elijah <ojetundeelijah2020@gmail.com>
2025-09-05 04:18:16 -0700

    Methods in java

---
+++ Method.java
@@ -0,0 +1,23 @@
+
+
+public class Method
+{
+    public static void main(String a[])
+    {
+        Computer comp = new Computer();
+        comp.playMusic();
+    }
+}
+
+class Computer
+{
+    public void playMusic()
+    {
+        System.out.println("Playing Music");
+    }
+
+    public String getMeAPen()
+    {
+        return "Pen";
+    }
+}
