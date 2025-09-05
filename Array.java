da3d3576cc2750d8587f7a01c7f3fbd9efdf7200
Ojetunde Elijah <ojetundeelijah2020@gmail.com>
2025-09-05 12:43:44 -0700

    Optimized solution to the question

--- Array.java
+++ Array.java
@@ -1,31 +1,31 @@
 public class ArrayOperations{
 
-    public int[] findEven(int arr[]){
+    public int[] reverseArray(int arr[]){
         int n = arr.length;
-        int oddCount = 0;
-        for(int i = 0 ; i< n; i++){
-            if(arr[i] % 2 != 0){
-                oddCount++;    
-            }
-            
+        int reversed[] = new int[n];
+        for (int i = 0; i < n; i++){
+            reversed[i] = arr[n - 1 - i];
         }
-        int[] result = new int[oddCount];
-        int idx = 0;
-        for(int i = 0; i < n; i++){
-            if(arr[i] % 2 != 0){
-                result[idx] = arr[i];
-                idx++;
-            }
+        return reversed;
+        
+    }
+    public int[] reverseArray2(int arr[], start, end){
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;  
         }
-        return result;
     }
 
     public static void main(String args[]){
         int arr[] = {1,2,3,4,5,6,7,8,9};
+        
         ArrayOperations ao = new ArrayOperations();
-        int evenArr[] = ao.findEven(arr);
-        for(int i = 0; i < evenArr.length; i++){
-            System.out.println(evenArr[i]);
+        int result[] = ao.reverseArray(arr);
+        for(int i = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
         }
     }
 }
