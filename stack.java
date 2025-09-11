de2cc9eb1df9663aae6ff131e4e864f59851b9a7
Ojetunde Elijah <ojetundeelijah2020@gmail.com>
2025-09-11 15:51:37 -0700

    reversing a string using a stack

--- stack.java
+++ stack.java
@@ -30,6 +30,16 @@ public class Stack{
         length ++;
     }
 
+    public int pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        int result = top.data;
+        top = top.next;
+        length--;
+        return result;
+    }
+
     public static void main(String args[]){
 
     }
