2fcf090298a30de33ba4c9fbdeeef55dde8c7050
Ojetunde Elijah <ojetundeelijah2020@gmail.com>
2025-09-11 15:33:22 -0700

    I implemented stacks

---
+++ stack.java
@@ -0,0 +1,36 @@
+public class Stack{
+    private ListNode top;
+    private int length;
+
+    private class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+        }
+    }
+
+    public Stack(){
+        top = null;
+        length = 0;
+    }
+
+    public int length(){
+        return length;
+    }
+    public boolean isEmpty(){
+        return length == 0;
+    }
+
+    public void push(int data){
+        ListNode temp = new ListNode(data);
+        temp.next =top;
+        top = temp;
+        length ++;
+    }
+
+    public static void main(String args[]){
+
+    }
+}
